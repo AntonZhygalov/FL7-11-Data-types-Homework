bf4c5a04825bc64b16410e0cf05efec9756954af
Anton <antonzhygalov@gmail.com>
2017-07-25 10:21:52 +0300

    Modified

--- js/decypherPhrase.js
+++ js/decypherPhrase.js
@@ -12,23 +12,20 @@ function getTransformedArray(arr, fn) {
     return newArr;
 }
 
-function cypherPhrase (obj, str) {
-     return getTransformedArray(str, function(a) {
-            for (var key in obj) {
-                if (obj.hasOwnProperty(a)) {
-                    return a = obj[a];
-                } else return a;   
-            }
-        }).join('');
+function cypherPhrase(obj, str) {
+    return getTransformedArray(str, function(a) {
+        for (var key in obj) {
+            if (obj.hasOwnProperty(a)) {
+                return a = obj[a];
+            } else return a;
+        }
+    }).join('');
 }
 
-function decypherPhrase (obj, str) {
-     return getTransformedArray(str, function(a) {
-            for (var key in obj) {
-                if (a === obj[key]) {
-                    return a = key;
-                } 
-            }
-            return a;
-        }).join('');
+function decypherPhrase(obj, str) {
+    var newObj = {};
+    for (var prop in obj) {
+        newObj[obj[prop]] = prop;
+    }
+    return cypherPhrase(newObj, str);
 }
